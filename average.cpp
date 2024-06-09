8253bc3155a2d0649ba50cb7278a75dd529fe9dd
Avnish Rana <avnishrana797@gmail.com>
2024-06-10 01:15:45 +0530

    Delete average.cpp

--- average.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a,b,c;
-    cin>>a>>b>>c; //taking values from user input
-    cout<<(a+b+c)/3<<endl; //printing average values
-    return 0;
-}
