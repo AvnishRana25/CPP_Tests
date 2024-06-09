bfa8b61f8f25ca89b836d0bd15dbcd3a1763f5a9
AvnishRana25 <avnishrana797@gmail.com>
2024-06-10 01:04:47 +0530

    "final patterns commit"

--- patterns/pattern10.cpp
+++ patterns/pattern10.cpp
@@ -9,23 +9,15 @@
 using namespace std;
 int main(){
     int n;
+    int a = 1;
     cin>>n;
-    int a[n][n];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++){
-            if(j==0){
-                a[i][j]=i+1;
-            }
-            else{
-                a[i][j]=a[i-1][j-1]+a[i][j-1];
-            }
-        }
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++){
-            cout<<a[i][j]<<" ";
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<< a <<" ";
+            a++;
         }
         cout<<endl;
     }
     return 0;
+ 
 }
